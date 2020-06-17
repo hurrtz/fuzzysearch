cd58cc2e989d51b8c53c98c9ef62100c155bf5f4
Tobias Winkler <info@tobiaswinkler.berlin>
2020-06-17 18:48:22 +0200

    added basic styling
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import './index.css';
+import theme from './theme';
 import Home from './pages/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <Home />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );

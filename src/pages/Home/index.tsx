285b9c2f470b95f3c2c7fdd095ec90b233b82bb0
Tobias Winkler <info@tobiaswinkler.berlin>
2020-06-17 19:18:15 +0200

    fetched fruits with axios
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Fuzzyfind from '../../components/Fuzzyfind';
+import axios from 'axios';
 import './styles.css';
 
 const Home = () => {
+  const [fruits, setFruits] = useState(undefined);
+
+  useEffect(() => {
+    axios.get('/data.json').then(({ data: { fruits: fetchedFruits } }) => {
+      setFruits(fetchedFruits);
+    });
+  }, []);
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
